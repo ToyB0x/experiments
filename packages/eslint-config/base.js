b4dd146a667b081c966ce094346c86d44364bbab
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-06-30 19:56:38 +0900

    feat: add lint settings (#23)

---
+++ packages/eslint-config/base.js
@@ -0,0 +1,22 @@
+const { resolve } = require('node:path')
+
+const project = resolve(process.cwd(), 'tsconfig.json')
+
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: ['eslint:recommended', 'prettier', 'turbo'],
+  settings: {
+    'import/resolver': {
+      typescript: {
+        project,
+      },
+    },
+  },
+  ignorePatterns: [
+    // Ignore dotfiles
+    '.*.js',
+    'node_modules/',
+    'dist/',
+  ],
+}
